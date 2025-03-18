8c9be2183426c49aacc936030cf4638a92cddc95
LuanSilva17 <luan.alves@copasul.coop.br>
2025-03-18 08:30:06 -0400

    Criando metodos e mudando a linguagem de programação no context

---
+++ contexts/embarqueContext.js
@@ -0,0 +1,38 @@
+import { DATA } from '@/data/db.test';
+
+import React, { createContext, useMemo, useState } from 'react';
+
+
+const EmbarqueContext = createContext({});
+
+function EmbarqueProvider({ children }) {
+    const [ data, setData ] = useState(DATA)
+
+    function acceptOn(data) {
+        setData((prevData) => {
+            const indiceData = prevData.findIndex(i => i.id === data.id)
+
+            if(indiceData == -1) {
+                console.error("Algo deu errado");
+                return;
+            }
+
+            const novaArray = [...prevData];
+            const dateref = new Date();
+
+            novaArray[indiceData] = { ...novaArray[indiceData], accepted: true, acceptedAt: `${dateref.getFullYear()}/${dateref.getMonth()}/${dateref.getDay()} ${dateref.getHours()}:${dateref.getMinutes()}` };
+
+            return novaArray;
+        })
+    }
+
+    const value = useMemo(() => ({ data, acceptOn }), [data]);
+
+    return (
+        <EmbarqueContext.Provider value={value}>
+            { children }
+        </EmbarqueContext.Provider>
+    );
+}
+
+export { EmbarqueProvider, EmbarqueContext };
