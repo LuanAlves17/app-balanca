3705d432f2f46672392057f1907613312485eacc
LuanSilva17 <luan.alves@copasul.coop.br>
2025-03-18 07:23:42 -0400

    Subindo Context API

---
+++ contexts/embarqueContext.ts
@@ -0,0 +1,17 @@
+// import React, { Component, createContext, useContext, useState } from 'react';
+
+// import { ObjectRequestDTO } from '@/data/modeldraft/arch/ObjectRequestDTO';
+
+
+// const EmbarqueContext = createContext({});
+
+// function EmbarqueProvider({ children }: any) {
+
+
+
+//     return (
+//         <EmbarqueContext.Provider>
+//             { children }
+//         </EmbarqueContext.Provider>
+//     );
+// }
